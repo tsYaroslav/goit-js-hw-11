94d21390aa7f5dda2d33e103a5bf78c9798a7b66
Yaroslav Tsiapych <yaroslav121mail@gmail.com>
2021-12-26 22:46:13 +0200

    fix

---
+++ src/app/aapp.js
@@ -0,0 +1,92 @@
+import { getImages } from '../constants/api'
+import { refs } from '../constants/refs'
+import { makePhotoCards } from '../components/photoCards/photoCards'
+import Notiflix from 'notiflix'
+
+const store = {
+  page: 1,
+  query: '',
+}
+
+const inputCheck = (e) => {
+  const input = refs.searchInput
+  if (input.value.charAt(0) === ' ') {
+    input.value = ''
+  }
+}
+
+const clickSubmit = (e) => {
+  e.preventDefault()
+  refs.gallery.innerHTML = ''
+  refs.loadMoreBtn.classList.add('visually-hidden')
+
+  const query = refs.searchForm.elements.searchQuery.value
+  store.query = query
+  store.page = 1
+
+  if (!query) {
+    Notiflix.Notify.failure('Please, type your query...')
+    return
+  } else {
+    async function resFunc(query) {
+      try {
+        const res = await getImages(query)
+        const data = await res.data
+        if (data.totalHits) {
+          refs.loadMoreBtn.classList.remove('visually-hidden')
+          Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+          makePhotoCards(data.hits)
+          if (data.totalHits <= 40) {
+            refs.loadMoreBtn.classList.add('visually-hidden')
+          }
+        } else {
+          refs.gallery.innerHTML = ''
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          )
+        }
+      } catch (err) {
+        alert('ERROR: ' + err)
+      }
+    }
+    resFunc(query)
+  }
+}
+
+const clickLoadMore = (e) => {
+  const query = store.query
+  store.page += 1
+
+  async function resFunc(query) {
+    try {
+      const res = await getImages(query, store.page)
+      const data = res.data
+      if (data.hits.length) {
+        makePhotoCards(data.hits)
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+      } else {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        )
+        refs.loadMoreBtn.classList.add('visually-hidden')
+      }
+    } catch (err) {
+      alert('ERROR: ' + err)
+    }
+  }
+  resFunc(query)
+}
+
+export let clickSubmitEvent = refs.searchForm.addEventListener(
+  'submit',
+  clickSubmit
+)
+export let clickLoadMoreEvent = refs.loadMoreBtn.addEventListener(
+  'click',
+  clickLoadMore
+)
+
+export let inputCheckEvent = refs.searchInput.addEventListener(
+  'input',
+  inputCheck
+)
